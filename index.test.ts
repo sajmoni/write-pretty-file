778d050da61c253e795e3e64517085efda87a9b2
Simon Lagos <simon.lagos.sallhed@gmail.com>
2023-11-05 14:21:16 +0100

    Test prettier defaults

--- index.test.ts
+++ index.test.ts
@@ -21,3 +21,13 @@ test('write-pretty-file', async () => {
   const formattedFile = await fs.readFile(filePath, 'utf-8')
   expect(formattedFile).toEqual("console.log('hello world')\n")
 })
+
+test('Uses prettier defaults if no config', async () => {
+  const directory = temporaryDirectory()
+
+  const unFormatted = "console.log('hello world')"
+  const filePath = path.join(directory, 'directory/index.js')
+  await writePrettyFile(filePath, unFormatted)
+  const formattedFile = await fs.readFile(filePath, 'utf-8')
+  expect(formattedFile).toEqual('console.log("hello world");\n')
+})
